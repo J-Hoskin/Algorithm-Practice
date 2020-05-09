5160c0f853867adb819272567fa768e527181a78
James Hoskin <J_hoskin@outlook.com>
2020-05-09 22:40:39 +1000

    Clear driver class

--- src/Core.java
+++ src/Core.java
@@ -5,22 +5,5 @@ import static Factory.DataFactory.createMatrix;
 public class Core {
     public static void main(String[] args){
 
-        Node<Integer> head = new Node<>(6);
-        head.addToTail(5);
-        head.addToTail(3);
-        head.addToTail(12);
-        head.addToTail(8);
-        head.addToTail(5);
-
-        LinkedLists t = new LinkedLists();
-        t.removeDupes2(head);
-
-        Node currentNode = head;
-        while(currentNode != null){
-            System.out.println(currentNode.data);
-            currentNode = currentNode.next;
-        }
-
-
     }
 }
